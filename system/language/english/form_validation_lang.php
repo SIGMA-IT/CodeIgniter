61d27e726c07daaa0fd04d6112dfc7a29cd89f35
gpittau <gpittau@gmail.com>
2011-12-26 18:35:23 -0300

    localizaciion spanish de form_validation

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -1,24 +1,45 @@
 <?php
 
+/*
+
 $lang['required'] 			= "The %s field is required.";
 $lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "The %s field must contain a valid email address.";
-$lang['valid_emails'] 		= "The %s field must contain all valid email addresses.";
+$lang['valid_email']                    = "The %s field must contain a valid email address.";
+$lang['valid_emails']                   = "The %s field must contain all valid email addresses.";
 $lang['valid_url'] 			= "The %s field must contain a valid URL.";
 $lang['valid_ip'] 			= "The %s field must contain a valid IP.";
 $lang['min_length']			= "The %s field must be at least %s characters in length.";
 $lang['max_length']			= "The %s field can not exceed %s characters in length.";
-$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['exact_length']                   = "The %s field must be exactly %s characters in length.";
 $lang['alpha']				= "The %s field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
+$lang['alpha_numeric']                  = "The %s field may only contain alpha-numeric characters.";
 $lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
 $lang['numeric']			= "The %s field must contain only numbers.";
 $lang['is_numeric']			= "The %s field must contain only numeric characters.";
 $lang['integer']			= "The %s field must contain an integer.";
 $lang['matches']			= "The %s field does not match the %s field.";
 $lang['is_natural']			= "The %s field must contain only positive numbers.";
-$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
-
+$lang['is_natural_no_zero']             = "The %s field must contain a number greater than zero.";
+*/
 
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/english/form_validation_lang.php */
+
+$lang['required'] 			= "El campo %s es requerido.";
+$lang['isset']				= "El campo %s debe tener un valor.";
+$lang['valid_email']                    = "El campo %s debe contener una direccion valida de email.";
+$lang['valid_emails']                   = "El campo %s debe contener todas las direcciones de email validas.";
+$lang['valid_url'] 			= "El campo %s debe contener una direccion URL valida.";
+$lang['valid_ip'] 			= "El campo %s debe contener una direccion IP valida.";
+$lang['min_length']			= "El campo %s debe tener al menos %s caracteres de largo.";
+$lang['max_length']			= "El campo %s no puede exceder los %s carcateres de largo.";
+$lang['exact_length']                   = "El campo %s debe tener exactamente %s caracteres de largo.";
+$lang['alpha']				= "El campo %s solo puede contener caracteres alfabeticos.";
+$lang['alpha_numeric']                  = "El campo %s solo puede contener caracteres alfa-numericos.";
+$lang['alpha_dash']			= "El campo %s solo puede contener caracteres alfa-numericos, guiones bajos, y barras.";
+$lang['numeric']			= "El campo %s debe contener solo numeros.";
+$lang['is_numeric']			= "El campo %s debe contener solo caracteres numericos.";
+$lang['integer']			= "El campo %s debe contener un valor entero.";
+$lang['matches']			= "El campo %s no concuerda con el campo %s.";
+$lang['is_natural']			= "El campo %s debe contener solo numeros positivos.";
+$lang['is_natural_no_zero']             = "El campo %s debe contener un numero mayor a cero.";
